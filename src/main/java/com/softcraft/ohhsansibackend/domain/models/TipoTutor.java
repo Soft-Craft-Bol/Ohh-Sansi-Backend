22685083dc2fe9ab8ac618ba4fc8d9177d504df3
alfredoT7 <123456alfredotorri@gmail.com>
2025-03-21 00:50:22 -0400

    feat: tipo tutor done

--- src/main/java/com/softcraft/ohhsansibackend/domain/models/TipoTutor.java
+++ src/main/java/com/softcraft/ohhsansibackend/domain/models/TipoTutor.java
@@ -7,4 +7,19 @@ public class TipoTutor {
     @NotNull(message = "El nombre del tipo de tutor no puede ser nulo")
     private String nombreTipoTutor;
 
+    public Long getIdTipoTutor() {
+        return idTipoTutor;
+    }
+
+    public void setIdTipoTutor(Long idTipoTutor) {
+        this.idTipoTutor = idTipoTutor;
+    }
+
+    public String getNombreTipoTutor() {
+        return nombreTipoTutor;
+    }
+
+    public void setNombreTipoTutor(String nombreTipoTutor) {
+        this.nombreTipoTutor = nombreTipoTutor;
+    }
 }
