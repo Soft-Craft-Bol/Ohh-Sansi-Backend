c054fbe64d6f5adbea2f8cea5878355564c6ef28
alfredoT7 <123456alfredotorri@gmail.com>
2025-03-21 02:12:10 -0400

    implementation of a Interface to crud IDatabase

--- src/main/java/com/softcraft/ohhsansibackend/domain/repository/abstraction/IDatabaseRepository.java
+++ src/main/java/com/softcraft/ohhsansibackend/domain/repository/abstraction/IDatabaseRepository.java
@@ -3,16 +3,40 @@ package com.softcraft.ohhsansibackend.domain.repository.abstraction;
 import java.util.*;
 
 public interface IDatabaseRepository<T, ID> {
-    T save(T entity);
-    List<T> saveAll(Iterable<T> entities);
-    Optional<T> findById(ID id);
-    boolean existsById(ID id);
-    List<T> findAll();
-    List<T> findAllById(Iterable<ID> ids);
-    long count();
-    void deleteById(ID id);
-    void delete(T entity);
-    void deleteAllById(Iterable<? extends ID> ids);
-    void deleteAll(Iterable<? extends T> entities);
-    void deleteAll();
+    default T save(T entity) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default List<T> saveAll(Iterable<T> entities) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default Optional<T> findById(ID id) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default boolean existsById(ID id) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default List<T> findAll() {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default List<T> findAllById(Iterable<ID> ids) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default long count() {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default void deleteById(ID id) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default void delete(T entity) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default void deleteAllById(Iterable<? extends ID> ids) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default void deleteAll(Iterable<? extends T> entities) {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
+    default void deleteAll() {
+        throw new UnsupportedOperationException("sin implementacion");
+    }
 }
