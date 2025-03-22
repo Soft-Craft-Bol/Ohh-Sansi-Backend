d62f04ad08f6af1d91759509c9543422a3de9e9f
alfredoT7 <123456alfredotorri@gmail.com>
2025-03-21 22:53:02 -0400

    Idatabase resolved 2

--- src/main/java/com/softcraft/ohhsansibackend/area/domain/repository/abstraction/IAreaRepository.java
+++ src/main/java/com/softcraft/ohhsansibackend/area/domain/repository/abstraction/IAreaRepository.java
@@ -1,6 +1,8 @@
 package com.softcraft.ohhsansibackend.area.domain.repository.abstraction;
 
 import com.softcraft.ohhsansibackend.area.domain.models.Area;
+import com.softcraft.ohhsansibackend.domain.IDatabaseRepository;
+
 
 public interface IAreaRepository extends IDatabaseRepository<Area, Integer> {
     boolean update(Area area);
