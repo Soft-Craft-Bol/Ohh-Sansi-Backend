25eb457c8201a87ea4098e05f9df3e7ddaa95983
alfredoT7 <123456alfredotorri@gmail.com>
2025-03-30 17:44:02 -0400

    select SQL

--- src/main/java/com/softcraft/ohhsansibackend/nivelescolar/application/usecases/AreaNivelEscolarService.java
+++ src/main/java/com/softcraft/ohhsansibackend/nivelescolar/application/usecases/AreaNivelEscolarService.java
@@ -3,8 +3,14 @@ package com.softcraft.ohhsansibackend.nivelescolar.application.usecases;
 
 import com.softcraft.ohhsansibackend.area.domain.models.AreaNivelEscolar;
 import com.softcraft.ohhsansibackend.nivelescolar.domain.repository.implementation.AreaNivelEscolarDomainRepository;
+import com.softcraft.ohhsansibackend.nivelescolar.dto.AreaCategoriaNivelDTO;
+import com.softcraft.ohhsansibackend.nivelescolar.dto.AreaNivelDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class AreaNivelEscolarService {
@@ -19,4 +25,18 @@ public class AreaNivelEscolarService {
     public AreaNivelEscolar saveAreaNivelEscolar(AreaNivelEscolar areaNivelEscolar) {
         return areaNivelEscolarDomainRepository.save(areaNivelEscolar);
     }
+
+    public Map<String, Object> getAreasAndCategoriasByNivel(int idNivel) {
+        List<AreaNivelDTO> areas = areaNivelEscolarDomainRepository.findAreasByNivel(idNivel);
+        List<AreaCategoriaNivelDTO> areasCategorias = areaNivelEscolarDomainRepository.findAreasCategoriasByNivel(idNivel);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("areas", areas);
+        result.put("areasCategorias", areasCategorias);
+
+        return result;
+    }
+
+
+
 }
